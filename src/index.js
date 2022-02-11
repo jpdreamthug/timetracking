85a6eb6ce5d8e7853f11a59dc75cd12c68cb9b99
jpdreamthug <lentorilp@gmail.com>
2022-02-11 14:17:12 +0100

    fix: add switcher

--- src/index.js
+++ src/index.js
@@ -4,47 +4,67 @@ import Image from './assets/images/image-jeremy.png'
 
 const card = `
 <div class="main_card">
-    <img src="./image-jeremy.png">
-    <span class="card_report">Report for</span>
-    <span class="card_jeremy">Jeremy Robson</span>
-</div>
-`
-const workCard = `
-<div class="cards work_card">
-    <span>work</span>
-
-</div>
-`
-const studyCard = `
-<div class="cards study_card">
-    <span>Study</span>
-
-</div>
-`
-const playCard = `
-<div class="cards play_card">
-    <span>Play</span>
-
-</div>
-`
-const socialCard = `
-<div class="cards social_card">
-    <span>Social</span>
-
-</div>
-`
-const selfCard = `
-<div class="cards self_card">
-    <span>Self Care</span>
-
+    <div class="main_info">
+        <img src="./image-jeremy.png">
+        <span class="card_report">Report for</span>
+        <span class="card_jeremy">Jeremy Robson</span>
+    </div>
+    <div class="switcher">
+        <span class="daily">Daily</span>
+        <span class="weekly">Weekly</span>
+        <span class="monthly">Monthly</span>
+    </div>
 </div>
 `
 const exsCard = `
 <div class="cards exercise_card">
-    <span>Exercise</span>
-
+    <span>.</span>
+    <div class="exs_info card_info">
+        <span>Exercise</span>
+    </div>
 </div>
 `
+const workCard = `
+<div class="cards work_card">
+    <span>.</span>
+    <div class="work_info card_info">
+        <span>Work</span>
+    </div>
+</div>
+`
+const studyCard = `
+<div class="cards study_card">
+    <span>.</span>
+    <div class="study_info card_info">
+        <span>Study</span>
+    </div>
+</div>
+`
+const playCard = `
+<div class="cards play_card">
+    <span>.</span>
+    <div class="play_info card_info">
+        <span>Play</span>
+    </div>
+</div>
+`
+const socialCard = `
+<div class="cards social_card">
+    <span>.</span>
+    <div class="social_info card_info">
+        <span>Social</span>
+    </div>
+</div>
+`
+const selfCard = `
+<div class="cards self_card">
+    <span>.</span>
+    <div class="self_info card_info">
+        <span>Self Care</span>
+    </div>
+</div>
+`
+
 const container = `
 <div class='container'>   
     ${card}
